68fe53f75fd573f79e9a86d839f16ea25d709185
cs <siwon.cezary@gmail.com>
2023-07-13 19:10:41 +0200

    Circle class definition

--- src/main/java/pl/sda/pol144/day2/ShapeDemo.java
+++ src/main/java/pl/sda/pol144/day2/ShapeDemo.java
@@ -3,8 +3,10 @@ package pl.sda.pol144.day2;
 public class ShapeDemo {
     public static void main(String[] args) {
         Shape[] picture = new Shape[5];
-
-
+        picture[0] = new Rectangle("r1", 20, 30);
+        picture[1] = new Rectangle("r2", 10, 30);
+        //picture[2] = new Shape("c1", 15);
+        drawPicture(picture);
     }
 
     public static void drawPicture(Shape[] picture){
