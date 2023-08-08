b44b5eeff4af5144123ecbc3757c29542d9068ac
cs <siwon.cezary@gmail.com>
2023-08-08 20:37:42 +0200

    ComparatorDemo add

--- src/main/java/pl/sda/pol144/day8/ComparatorDemo.java
+++ src/main/java/pl/sda/pol144/day8/ComparatorDemo.java
@@ -1,6 +1,8 @@
 package pl.sda.pol144.day8;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ComparatorDemo {
@@ -8,8 +10,19 @@ public class ComparatorDemo {
         List<Customer> customerList = List.of(
                 new Customer(1, "Ewa", LocalDate.of(2000,10,10)),
                 new Customer(2, "Adam", LocalDate.of(2001,11,10)),
-                new Customer(3, "Karol", LocalDate.of(1999,10,11))
+                new Customer(3, "Karol", LocalDate.of(1999,10,11)),
+                new Customer(3, "Ewa", LocalDate.of(1998,1,1))
         );
-        customerList.stream().sorted().forEach(System.out::println);
+        Comparator<Customer> byName = (c1, c2) -> c1.name.compareTo(c2.name);
+        Comparator<Customer> byBirth = Comparator.comparing(c -> c.birth);
+        Comparator<Customer> byNameAndByBirth = byName.thenComparing(byBirth);
+        System.out.println("Sortowanie wg imion i dat urodzin: ");
+        customerList.stream().sorted(byNameAndByBirth).forEach(System.out::println);
+        // utwórz komparator dla pola birth
+        Comparator<Customer> byId = Comparator.comparing(c -> c.id);
+        customerList.stream().sorted(byBirth.reversed()).forEach(System.out::println);
+        customerList = new ArrayList<>(customerList);
+        customerList.sort(byBirth);     // posortowanie listy w porządku komparatora byBirth
+        System.out.println(customerList);
     }
 }
