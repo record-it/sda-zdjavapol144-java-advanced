0a16cfa8d54906852a006949413eb9a1dddc89ca
cs <siwon.cezary@gmail.com>
2023-08-09 19:03:24 +0200

    OneThreadDemo.java add

--- src/main/java/pl/sda/pol144/day9/TwoThreadDemo.java
+++ src/main/java/pl/sda/pol144/day9/TwoThreadDemo.java
@@ -1,4 +1,29 @@
 package pl.sda.pol144.day9;
 
+import java.util.Scanner;
+
 public class TwoThreadDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Thread thread = new Thread(() ->
+        {
+            while(true){
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        );
+        // uruchamiamy wątek roboczy
+        thread.start();
+        // wątek main, obsługa zdarzeń
+        while(true){
+            if (scanner.next().equals("q")){
+                break;
+            }
+        }
+    }
 }
